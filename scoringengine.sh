27015f114623f1b2021ee77675c07bbbc6703baf
Martin104921 <michael@wshscyberpatriot.com>
2015-11-15 15:28:21 -0500

    Update scoringengine.sh
    Added ufw problems for 5 points each

--- scoringengine.sh
+++ scoringengine.sh
@@ -5,6 +5,22 @@ else
   echo "you are not root"
 	exit 1
 fi
-echo "Welcome to the Cyberpatriot Round One Replica!"
-echo "Test add"
-echo "test two"
+score=0
+ufw status | grep inactive
+if [ $? -ne 0 ]; then
+    let "score += 5"
+else
+  echo "you missed ufw being off" >> /misseditems
+  ufw status >> /misseditems
+  ufw enable
+fi
+ufw status verbose | grep Logging | grep -v high
+if [ $? -ne 0 ]; then
+  let "score += 5"
+else
+  echo "you missed setting ufw logging to high" >> /misseditems
+  ufw status verbose | grep Logging >> /misseditems
+  ufw logging high
+fi
+echo "$score"
+
