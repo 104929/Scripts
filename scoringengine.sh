0373de2507d95e49524bcc2223b06e222d4d3a28
Simon Dickerman <simondickerman@gmail.com>
2015-11-17 01:23:36 -0500

    Issue Order, Comments, if fixes
    Ordered the issues in the order they appear on the scoring report.
    Added comments to indicate where a section of code will go in the
    future.
    Reverted the if [ $? -eq  0 ]; then to if [ $? -eq = 0 ]; then
    .

--- scoringengine.sh
+++ scoringengine.sh
@@ -5,7 +5,68 @@ else
   echo "you are not root"
 	exit 1
 fi
+
 score=0
+
+#Forensics Question 1 correct - 8 pts
+
+#Forensics Question 2 correct - 8 pts
+
+#Created user abracadaniel - 5 pts
+
+#Removed unauthorized user gunter - 4 pts
+tail /etc/shadow | grep gunter | grep "!"
+if [ $? -eq = 0 ]; then
+	let "score += 4"
+	echo "Removed unauthorized user gunter +4"
+else
+	echo "you missed locking out gunter from logging in" >> /misseditems
+	tail /etc/shadow | grep gunter >> /misseditems
+fi
+
+#Removed unauthorized user hunsonadeer - 4 pts
+tail /etc/shadow | grep hunsonadeer | grep "!"
+if [ $? -eq = 0 ]; then
+	let "score += 4"
+	echo "Removed unauthorized user hunsonadeer +4"
+else
+	echo "you missed locking out hunsonadeer from logging in" >> /misseditems
+	tail /etc/shadow | grep hunsonadeer >> /misseditems
+fi
+
+#User lumpyspaceprincess is not an administrator - 4 pts
+#User peppermintbutler is not an administrator - 4 pts
+# group check section (needs fixing)
+# lumpyspaceprincess=$(less /etc/group | grep "lumpyspaceprincess")
+# lumpyspaceprincesssudo=$(grep sudo $lumpyspaceprincess)
+# lumpyspaceprincessadm=$(grep adm $lumpyspaceprincess)
+# if [ $lumpyspaceprincesssudo -eq 0 ]; then
+# 	if [ $lumpyspaceprincessadm -eq 0 ]; then
+# 		let "score += 4"
+# 		echo "User lumpyspaceprincess is not an administrator"
+# 	fi
+# else
+# 	echo "lumpyspaceprincess is an admin" >> /misseditems
+# 	#tail /etc/shadow | grep miller >> /misseditems
+# 	#passwd -l miller
+
+#Changed insecure root password - 4 pts
+
+#Guest account is disabled - 6 pts
+
+#A default minimum password age is set - 6 pts
+
+#Install updates from important security updates - 7 pts
+
+#Linux kernel has been updated - 7 pts
+
+#Sudo has been updated - 7 pts
+
+#Prohibited MP3 files are removed - 6 pts
+
+#DNS is disabled or removed - 6 pts
+
+#Firewall has been enabled - 5 pts
 ufw status | grep inactive
 if [ $? -ne 0 ]; then
     let "score += 5"
@@ -15,33 +76,9 @@ else
   ufw status >> /misseditems
   ufw enable
 fi
-tail /etc/shadow | grep gunter | grep "!"
-if [ $? -eq 0 ]; then
-	let "score += 4"
-	echo "Removed unauthorized user gunter +4"
-else
-	echo "you missed locking out gunter from logging in" >> /misseditems
-	tail /etc/shadow | grep gunter >> /misseditems
-fi
-tail /etc/shadow | grep hunsonadeer | grep "!"
-if [ $? -eq 0 ]; then
-	let "score += 4"
-	echo "Removed unauthorized user hunsonadeer +4"
-else
-	echo "you missed locking out hunsonadeer from logging in" >> /misseditems
-	tail /etc/shadow | grep hunsonadeer >> /misseditems
-fi
-lumpyspaceprincess=$(less /etc/group | grep "lumpyspaceprincess")
-lumpyspaceprincesssudo=$(grep sudo $lumpyspaceprincess)
-lumpyspaceprincessadm=$(grep adm $lumpyspaceprincess)
-if [ $lumpyspaceprincesssudo -eq 0 ]; then
-	if [ $lumpyspaceprincessadm -eq 0 ]; then
-		let "score += 4"
-		echo "User lumpyspaceprincess is not an administrator"
-	fi
-else
-	echo "lumpyspaceprincess is an admin" >> /misseditems
-	#tail /etc/shadow | grep miller >> /misseditems
-	#passwd -l miller
+
+#SSH root login has been disabled - 7 pts
+
+
 fi
 echo "$score"
