baf57d58198691806ecaf818bdacaacae601b4c4
LoganGrant <Logan00084@gmail.com>
2016-02-19 08:28:45 -0500

    Fixed Pam section

--- Linux Scripts/SomethingObscure.sh
+++ Linux Scripts/SomethingObscure.sh
@@ -311,7 +311,7 @@ echo
 echo "Securing Login.defs now"
 sed -i 's/LOG_OK_LOGINS           no/LOG_OK_LOGINS           yes/g' /etc/login.defs
 sed -i 's/PASS_MIN_DAYS.*/PASS_MIN_DAYS   10/g' /etc/login.defs
-sed -i 's/PASS_MAX_DAYS.*/PASS_MAX_DAYS 90/g' /etc/login.defs
+sed -i 's/PASS_MAX_DAYS.*/PASS_MAX_DAYS   90/g' /etc/login.defs
 sed -i 's/LOGIN_RETRIES.*/LOGIN_RETRIES           3/g' /etc/login.defs
 sed -i 's/PASS_WARN_AGE.*/PASS_WARN_AGE   7/g' /etc/login.defs
 echo "Login.defs is now secure"
@@ -323,10 +323,10 @@ grep "auth 	required 			pam_tally.so deny=5 onerr=fail unlock_time=1800 audit ev
 if [ "$?" -eq "1" ]; then
   echo "auth 	required 			pam_tally.so deny=5 onerr=fail unlock_time=1800 audit even_deny_root_account silent" >> /etc/pam.d/common-auth
 fi
-grep "password 	requisite 			pam_cracklib.so" /etc/pam.d/common-password >> /dev/null
+grep "pam_cracklib.so retry=3 minlen=8 difok=3 ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1" /etc/pam.d/common-password >> /dev/null
 if [ "$?" -eq "1" ]; then
-  sed -i 's/password 	requisite 			pam_cracklib.so.*/password 	requisite 			pam_cracklib.so retry=3 minlen=8 difok=3 ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1/g' /etc/pam.d/common-password
-  sed -i 's/password 	[success=1 default=ignore] 	pam_unix.so obscure use_authtok try_first_pass sha512.*/password 	[success=1 default=ignore] 	pam_unix.so obscure use_authtok try_first_pass sha512 minlen=8 remember=5/g' /etc/pam.d/common-password
+  sed -i 's/pam_cracklib.so.*/password 	requisite 			pam_cracklib.so retry=3 minlen=8 difok=3 ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1/g' /etc/pam.d/common-password
+  sed -i 's/pam_unix.so.*/password 	[success=1 default=ignore] 	pam_unix.so obscure use_authtok try_first_pass sha512 minlen=8 remember=5/g' /etc/pam.d/common-password
 fi
 echo
 echo "Editing Sysctl.conf now"
